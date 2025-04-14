997b194c399760f9fe443c11e4a044f4c174a161
bouda890 <bouda890@seznam.cz>
2025-04-14 19:00:35 +0200

    last update

--- main/run.py
+++ main/run.py
@@ -28,7 +28,7 @@ class VelocityControl:
         self.velocity = 0
         self.max_acc = 0.5 # m/s^2
         self.max_ang_acc = 0.5 # rad/s^2
-        self.max_speed = 0.5 # m/s #1.5
+        self.max_speed = 0.6 # m/s #1.5
         self.max_ang_speed = 0.6 # rad/s
         self.last_cmd = (0, 0)
         self.ang_p = 1.5
