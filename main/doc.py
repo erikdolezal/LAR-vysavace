3fea8257e7d9177394b3bc81f48508b8d888f264
bouda890 <bouda890@seznam.cz>
2025-04-16 14:02:55 +0200

    kjl

--- main/doc.py
+++ main/doc.py
@@ -1,4 +1,4 @@
 import pdoc
 from pathlib import Path
 pdoc.render.configure(docformat = "google")
-pdoc.pdoc("run", "algorithms", "configs", output_directory = Path("doc"))
+pdoc.pdoc("run", "algorithms", "configs", "SimMain", "Sim", output_directory = Path("doc"))
